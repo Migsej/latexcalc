827a05ee94f01f187475f195503a204a35bf2165
Migsej <vincent@pinkwin.home>
2023-08-18 09:38:31 +0200

    im so happy

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use pest::{Parser, iterators::Pair};
 use pest_derive::Parser;
-use calculator::Operation;
+use calculator::{Operation, evaluator::evaluate};
 use anyhow::Result;
 
 #[derive(Parser)]
@@ -13,42 +13,54 @@ pub fn parse(equation: String) -> Result<Vec<Operation>> {
 
 }
 
+pub fn handlerule(operation: Pair<'_, Rule>) -> Result<Vec<Operation>> {
+    let mut ops = Vec::new();
+
+    match operation.as_rule() {
+        Rule::number =>  {
+            let number: f64 = operation 
+                .as_str()
+                .parse::<f64>()?;
+            ops.push(Operation::Number(number));
+
+        },
+        Rule::multiply => ops.push(Operation::Multiply),
+        Rule::plus => ops.push(Operation::Plus),
+        Rule::minus => ops.push(Operation::Minus),
+        Rule::fraction => {
+            let mut inner_rules = operation.into_inner();
+
+            let bla1 = inner_rules.next().unwrap();
+            println!("{}", bla1.clone().into_inner());
+
+            let bla2 = inner_rules.next().unwrap();
+
+            let mut first = handlerule(bla1)?;
+            let mut last = handlerule(bla2)?;
+
+
+            ops.push(Operation::OpenParenthesis);
+            ops.append(&mut first);
+            ops.push(Operation::ClosedParenthesis);
+            ops.push(Operation::Divide);
+            ops.push(Operation::OpenParenthesis);
+            ops.append(&mut last);
+            ops.push(Operation::ClosedParenthesis);
+
+        },
+        _ => unreachable!(),
+    }
+    Ok(ops)
+
+}
+
 pub fn handleparsed(equation: Pair<'_, Rule>) -> Result<Vec<Operation>> {
 
     let mut ops = Vec::new();
 
     for operation in equation.into_inner() {
-        match operation.as_rule() {
-            Rule::number =>  {
-                let number: f64 = operation 
-                    .as_str()
-                    .parse::<f64>()?;
-                ops.push(Operation::Number(number));
-
-            },
-            Rule::multiply => ops.push(Operation::Multiply),
-            Rule::fraction => {
-                let mut inner_rules = operation.into_inner();
-
-                let bla1 = inner_rules.next().unwrap();
-
-                let bla2 = inner_rules.next().unwrap();
-
-                //TODO this is stupid
-                let mut first = parse(bla1.as_str().to_string())?;
-                let mut last = parse(bla2.as_str().to_string())?;
-
-                ops.push(Operation::OpenParenthesis);
-                ops.append(&mut first);
-                ops.push(Operation::ClosedParenthesis);
-                ops.push(Operation::Divide);
-                ops.push(Operation::OpenParenthesis);
-                ops.append(&mut last);
-                ops.push(Operation::ClosedParenthesis);
-
-            },
-            _ => unreachable!(),
-        }
+        let mut bla = handlerule(operation)?;
+        ops.append(&mut bla);
             
     }
     Ok(ops)
@@ -71,6 +83,13 @@ mod tests {
         let latex = r"2*\frac{2}{2}".to_string();
         let out = vec![Operation::Number(2.0), Operation::Multiply, Operation::OpenParenthesis, Operation::Number(2.0), Operation::ClosedParenthesis, Operation::Divide, Operation::OpenParenthesis, Operation::Number(2.0), Operation::ClosedParenthesis];
         assert_eq!(parse(latex)?, out);
+        Ok(())
+    }
+
+    #[test]
+    fn evaluate_full() -> Result<()> {
+        let latex = r"2*\frac{2}{2}".to_string();
+        assert_eq!(evaluate(parse(latex)?)?, 2.0);
         Ok(())
     }
 
