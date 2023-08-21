807c6331a64ce69f58f76b6460f9acb55f700aa4
Migsej <vincent@pinkwin.home>
2023-08-21 13:43:26 +0200

    added a test

--- src/lib.rs
+++ src/lib.rs
@@ -144,6 +144,12 @@ mod tests {
         Ok(())
     }
     #[test]
+    fn evaluate_frac() -> Result<()> {
+        let latex = r"2*\frac{-5}{-2}".to_string();
+        assert_eq!(evaluate(parse(latex)?)?, 2.0);
+        Ok(())
+    }
+    #[test]
     fn evaluate_full() -> Result<()> {
         let latex = r"2*\frac{2}{2}".to_string();
         assert_eq!(evaluate(parse(latex)?)?, 2.0);
